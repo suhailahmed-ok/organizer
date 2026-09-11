8a510ced687b2db670bb22f2feb6360ab4e3ed2d
suhail <sohail4hmed@gmail.com>
2026-09-11 20:37:09 +0530

    add application test

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import requests
 
-response = requests.get("https://example.com")
+
+def check_website():
+    response = requests.get("https://example.com")
+    return response.status_code
+
 
 print("dockerising done")
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,11 @@
-import requests
+from unittest.mock import patch
+from app import check_website
 
-def test_request_installed():
-    assert requests is not None
+
+@patch("app.requests.get")
+def test_check_website(mock_get):
+    mock_get.return_value.status_code = 200
+
+    result = check_website()
+
+    assert result == 200
